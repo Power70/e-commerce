1a76553f156e9a27c855d922682982a442ae9416
Power70 <nwisuanu@gmail.com>
2025-07-15 18:23:53 +0100

    prog: Setup environment variable and the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'test',
-      database: 'e-commerce',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot(),
+    
+TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    models: [],
+  }),
+  inject: [ConfigService],
+}),
+
   ],
   controllers: [AppController],
   providers: [AppService],
